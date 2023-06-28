9910809425cc0e295b193ac0f9e7147e8a803748
JinOptimist <88jin@mail.ru>
2023-06-28 19:44:49 +0300

    Update carusel

--- js/carusel.js
+++ js/carusel.js
@@ -1,14 +1,46 @@
 $(document).ready(function () {
     const timeAnimation = 2 * 1000;
 
-    $('img').click(function () {
+    const images = [
+        'images/logo.webp', //0
+        'images/Gats.jpg',//1
+        'images/HONDA.jpg',//2
+        'images/avatars/man.png',//3
+        'images/avatars/woman.jpg',//4
+    ];
+
+    let index = 0;
+
+    init();
+
+    $('.btn-next').click(function () {
+        index = cyrcleIndex(index + 1);
+        moveToImage('.next');
+    });
+
+    $('.btn-prev').click(function () {
+        index = cyrcleIndex(index - 1);
+        moveToImage('.prev');
+    });
+
+    $('.o-blocks a').click(function (){
+        const newIndex = $(this).attr('index') - 0;
+        index  = newIndex;
+        const imageUrl = images[index];
+        $('.next').attr('src', imageUrl);
+        moveToImage('.next');
+        
+        // updateImagesBaseOnIndex();
+    });
+
+    function moveToImage(blockSelector){
         $('.center').animate(
             {
                 width: 0
             },
             timeAnimation);
 
-        $('.next').animate(
+        $(blockSelector).animate(
             {
                 width: 100
             },
@@ -16,14 +48,49 @@ $(document).ready(function () {
             'swing',
             function () {
                 //At the end of animation
-                const urlFirstImage = $('.center').attr('src');
-                const urlSecondImage = $('.next').attr('src');
+                updateImagesBaseOnIndex();
 
-                $('.center').attr('src', urlSecondImage);
-                $('.next').attr('src', urlFirstImage);
                 $('.center').css('width', '100px');
-                $('.next').css('width', '0');
+                $(blockSelector).css('width', '0');
             });
-    });
+    }
 
+    function updateImagesBaseOnIndex(){
+        let prevIndex = cyrcleIndex(index - 1);
+        let nextIndex = cyrcleIndex(index + 1);
+
+        $('.prev').attr('src', images[prevIndex]);
+        $('.center').attr('src', images[index]);
+        $('.next').attr('src', images[nextIndex]);
+
+        $('.currentIndex').val(index);
+
+        $(`.o-blocks a`).text('o');
+        $(`.o-blocks a[index=${index}]`).text('x');
+    }
+
+    function cyrcleIndex(index){
+        let goodIndex = index;
+        if (goodIndex < 0){
+            goodIndex = images.length - 1;
+        }
+
+        if (goodIndex >= images.length){
+            goodIndex = 0;
+        }
+
+        return goodIndex;
+    }
+
+    function init(){
+        for (let i = 0; i < images.length; i++) {
+            //const oTag = $('<a>');
+            const oTagJquery = $(document.createElement('a'));
+            oTagJquery.text('o');
+            oTagJquery.attr('index', i);
+            $('.o-blocks').append(oTagJquery);
+        }
+
+        updateImagesBaseOnIndex();
+    }
 });
